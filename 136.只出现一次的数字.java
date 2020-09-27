9ff16288c64639b5668c97cd377e44f9f64bb341
LiuMinxuan <liuminxuan123@gmail.com>
2020-09-27 19:24:25 +0900

    2020.9.27

--- 136.只出现一次的数字.java
+++ 136.只出现一次的数字.java
@@ -7,7 +7,15 @@
 // @lc code=start
 class Solution {
     public int singleNumber(int[] nums) {
-
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i + 1]){
+                i++;
+            }else{
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
     }
 }
 // @lc code=end
